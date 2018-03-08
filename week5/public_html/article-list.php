087fdc3c1146e90129fffc204448eda27ac94ec3
webdevAndres <macias.andres02@gmail.com>
2018-03-08 11:46:06 -0600

    displayed list in a table using foreach loop

--- week5/public_html/article-list.php
+++ week5/public_html/article-list.php
@@ -5,8 +5,127 @@ $newsArticle = new NewsArticles();
 
 $articleList = $newsArticle->getList();
 
-var_dump($articleList);
+// var_dump($articleList);
 
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>article List</title>
+
+    <style>
+        body {
+            background: linear-gradient(141deg, #0fb8ad 0%, #1fc8db 51%, #2cb5e8 75%);
+            opacity: .95;
+            height: 100vh;
+        }
+
+        table {
+            background-color: white;
+        }
+
+        table,
+        th,
+        td {
+            border: 1px solid black;
+        }
+
+        th,
+        td {
+            padding: 15px;
+        }
+
+        th {
+            text-align: left;
+        }
+
+        table {
+            border-spacing: 5px;
+            margin: auto;
+        }
+
+        header {
+            text-align: center;
+            font-size: 2em;
+        }
+
+        td a {
+            text-decoration:none;
+            font-size: 20px;
+        }
+
+        .button {
+            background-color: white;
+            color: black;
+            border: 2px solid #4CAF50;
+        }
+
+        .button:hover {
+            background-color: #4CAF50;
+            color: white;
+        }
+    </style>
+
+</head>
+
+<body>
+    <header>
+        <h2>Articles</h2>
+    </header>
+
+<table>
+<tr>
+    <th> <a href="article-view.php">View articles</a></th>
+    <th><a href="article-edit.php">Add new article</a></th>
+</tr>
+
+</table>
+
+    <table>
+
+        <tr>
+            <th>Article Title</th>
+            <th>Article Content</th>
+            <th>Article Author</th>
+            <th>Article Date</th>
+            <th>Options</th>
+            
+        </tr>
+
+        <?php foreach ($articleList as $articleInfo) { ?>
+        <tr>
+            <td>
+                <?php echo $articleInfo['articleTitle'];?>
+            </td>
+            <td>
+                <?php echo $articleInfo['articleContent'];?>
+            </td>
+            <td>
+                <?php echo $articleInfo['articleAuthor'];?>
+            </td>
+            <td>
+                <?php echo $articleInfo['articleDate'];?>
+            </td>
+
+            <td class="button">
+                <a href="article-edit.php">Edit</a>
+            </td>
+            <td>
+                <?php echo $articleInfo['articleID'];?>
+
+            </td>
+           
+        </tr>
+
+        <?php } ?>
+    </table>
+
+</body>
+
+</html>
