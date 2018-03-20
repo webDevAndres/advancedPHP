17c60af632cdf1a720d538fd923916eb7ce60e3d
webdevAndres <macias.andres02@gmail.com>
2018-03-20 09:09:47 -0500

    moved to mvc and fixed minor typo

--- week5/public_html/article-edit.php
+++ week5/public_html/article-edit.php
@@ -11,6 +11,12 @@ if (isset($_REQUEST['articleID']) && $_REQUEST['articleID'] > 0) {
     $articleDataArray = $newsArticle->articleData;
 }
 
+if (isset($_POST['cancel']))
+{
+    header("location: article-list.php");
+    exit;
+}
+
 // apply the data if we have new data
 if (isset($_POST['save'])) {
     $articleDataArray = $_POST;
@@ -19,79 +25,26 @@ if (isset($_POST['save'])) {
     $articleDataArray = $newsArticle->sanitize($articleDataArray);
     $newsArticle->set($articleDataArray);
     //validate
-    if ($newsArticle->validate()) {
+    if ($newsArticle->validate()) 
+    {
         //  save
-        if ($newsArticle->save()) {
+        if ($newsArticle->save()) 
+        {
             header("location: article-save-success.php");
             exit;
-        } else {
+        } 
+        else 
+        {
             $articleErrorsArray[] = "Save failed";
         }
-    } else {
+    } 
+    else 
+    {
         $articleErrorsArray = $newsArticle->errors;
         $articleDataArray = $newsArticle->articleData;
     }
 
 }
+require_once('../tpl/article-edit.tpl.php');
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <form action="<?php echo $_SERVER['SCRIPT_NAME']; ?>" method="post">
-
-    <label for="">Title</label>
-    <?php if(isset($articleErrorsArray['articleTitle'])) 
-    {?>
-
-<div><?php echo $articleErrorsArray['articleTitle']?></div>
-
-    <?php } ?>
-
-    <input type="text" name="articleTitle" value="<?php echo (isset($articleDataArray['articleTitle']) ? $articleDataArray['articleTitle'] : '');?>"> <br>
-
-   
-
-    <label for="content">Content</label>
-    <?php if(isset($articleErrorsArray['articleContent'])) 
-    {?>
-
-<div><?php echo $articleErrorsArray['articleContent']?></div>
-
-    <?php } ?>
-    <textarea name="articleContent"><?php echo (isset($articleDataArray['articleContent']) ? $articleDataArray['articleContent'] : '');?></textarea><br>
-
-
-    <label for="">Author</label>
-    <?php if(isset($articleErrorsArray['articleAuthor'])) 
-    {?>
-
-<div><?php echo $articleErrorsArray['articleAuthor']?></div>
-
-    <?php } ?>
-    <input type="articleAuthor" name="articleAuthor" value="<?php echo (isset($articleDataArray['articleAuthor']) ? $articleDataArray['articleAuthor'] : '');?>"><br>
-
-
-    <label for="">Date</label>
-    <?php if(isset($articleErrorsArray['articleDate'])) 
-    {?>
-
-<div><?php echo $articleErrorsArray['articleDate']?></div>
-
-    <?php } ?>
-    <input type="articleDate" name="articleDate" value="<?php echo (isset($articleDataArray['articleDate']) ? $articleDataArray['articleDate'] : '');?>"><br>
-
-    <input type="hidden" name="articleID" value="<?php echo (isset($articleDataArray['articleID']) ? $articleDataArray['articleID'] : '');?>"><br>
-
-    <input type="submit" name="save" value="Save">
-    <input type="reset" name="cancel" value="Cancel">
-    </form>
-</body>
-</html>
