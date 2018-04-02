e431c5e25ebfcdf39f88f78363101e95e7633057
webdevAndres <macias.andres02@gmail.com>
2018-04-02 14:00:01 -0500

    created user class and updated newsarticle class

---
+++ week7/inc/NewsArticles.class.php
@@ -0,0 +1,167 @@
+<?php
+
+class NewsArticles
+{
+    // using var is the same as public
+    public $articleData = array();
+    public $errors = array();
+
+    public $db = null;
+
+     function __construct()
+    {
+        $this->db = new PDO('mysql:host=localhost;dbname=wdv441_2018;charset=utf8', 'wdv441', 'wdv441');
+    }
+
+     function set($dataArray)
+    {
+        $this->articleData = $dataArray;
+    }
+
+     function sanitize($dataArray)
+    {
+        //sanitize the data based on rules
+        // $dataArray['articleTitle'] = filter_var($dataArray['articleTitle'], FILTER_SANITIZE_STRING);
+        $dataArray['articleContent'] = filter_var($dataArray['articleContent'], FILTER_SANITIZE_STRING);
+        $dataArray['articleAuthor'] = filter_var($dataArray['articleAuthor'], FILTER_SANITIZE_STRING);
+        $dataArray['articleDate'] = filter_var($dataArray['articleDate'], FILTER_SANITIZE_STRING);
+
+        return $dataArray;
+    }
+
+     function load($articleID)
+    {
+
+        $isLoaded = false;
+        //load info from database
+        $stmt = $this->db->prepare("SELECT * FROM newsarticles WHERE articleID=?");
+        $stmt->execute(array($articleID));
+
+        if ($stmt->rowCount() == 1) {
+            $dataArray = $stmt->fetch(PDO::FETCH_ASSOC);
+            // var_dump($dataArray);
+            $this->set($dataArray);
+
+            $isLoaded = true;
+        }
+
+        // var_dump($stmt->rowCount());
+
+        return $isLoaded;
+    }
+
+    function save() 
+    {
+        $isSaved = false;
+        
+        // determine if insert or update based on articleID
+        // save data from articleData property to database
+        if (empty($this->articleData['articleID']))
+        {
+            $stmt = $this->db->prepare(
+                "INSERT INTO newsarticles 
+                    (articleTitle, articleContent, articleAuthor, articleDate) 
+                 VALUES (?, ?, ?, ?)");
+               
+            $isSaved = $stmt->execute(array(
+                    $this->articleData['articleTitle'],
+                    $this->articleData['articleContent'],
+                    $this->articleData['articleAuthor'],
+                    $this->articleData['articleDate']
+                )
+            );
+            
+            if ($isSaved) 
+            {
+                $this->articleData['articleID'] = $this->db->lastInsertId();
+            }
+        } 
+        else 
+        {
+            $stmt = $this->db->prepare(
+                "UPDATE newsarticles SET 
+                    articleTitle = ?,
+                    articleContent = ?,
+                    articleAuthor = ?,
+                    articleDate = ?
+                WHERE articleID = ?"
+            );
+                    
+            $isSaved = $stmt->execute(array(
+                    $this->articleData['articleTitle'],
+                    $this->articleData['articleContent'],
+                    $this->articleData['articleAuthor'],
+                    $this->articleData['articleDate'],
+                    $this->articleData['articleID']
+                )
+            );            
+        }
+                        
+        return $isSaved;
+    }
+
+     function validate()
+    {
+        $isValid = false;
+
+        // if an error, store to errors using column name as key
+
+        //validate the data elements and article data property
+        if (empty($this->articleData['articleTitle'])) {
+            $this->errors['articleTitle'] = "Please enter Content";
+        }
+        if (empty($this->articleData['articleContent'])) {
+            $this->errors['articleContent'] = "Please enter Content";
+        }
+        if (empty($this->articleData['articleAuthor'])) {
+            $this->errors['articleAuthor'] = "Please enter an Author";
+        } 
+        
+        $year = substr($this->articleData['articleDate'], 0, 4);
+        $month = substr($this->articleData['articleDate'], 5,2);
+        $day = substr($this->articleData['articleDate'], 8,2);
+
+        if (empty($this->articleData['articleDate'])) {
+            $this->errors['articleDate'] = "Please enter a Date";
+        }
+        elseif (!checkdate($month,$day,$year)) {
+            $this->errors['articleDate'] = "Enter a valid date (yyyy-mm-dd)";
+            
+        }
+
+        if(empty($this->errors)){
+            $isValid = true;
+        }
+        return $isValid;
+    }
+
+     function getList($sortColumn = null, $sortDirection = null, $filterColumn = null, $filterText = null)
+    {
+        $articleList = array();
+        $sql = "SELECT * FROM newsarticles ";
+
+        if(!is_null($filterColumn) && !is_null($filterText))
+        {
+            $sql .= "WHERE " . $filterColumn . " LIKE ?";
+        }
+
+        if(!is_null($sortColumn))
+        {
+            $sql .= "ORDER BY " . $sortColumn;
+            if(!is_null($sortDirection))
+            {
+                $sql .= " " . $sortDirection;
+            }
+        }
+        
+        $stmt = $this->db->prepare($sql);
+
+        if ($stmt) {
+            $stmt->execute(array('%' . $filterText . '%'));
+            
+            $articleList = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        return $articleList;
+    }
+}
---
+++ week7/inc/Users.class.php
@@ -0,0 +1,179 @@
+<?php
+
+class Users
+{
+    // using var is the same as public
+    public $userData = array();
+    public $errors = array();
+
+    public $db = null;
+
+     function __construct()
+    {
+        $this->db = new PDO('mysql:host=localhost;dbname=wdv441_2018;charset=utf8', 'wdv441', 'wdv441');
+    }
+
+     function set($dataArray)
+    {
+        $this->userData = $dataArray;
+    }
+
+     function sanitize($dataArray)
+    {
+        //sanitize the data based on rules
+        $dataArray['userName'] = filter_var($dataArray['userName'], FILTER_SANITIZE_STRING);
+        $dataArray['password'] = filter_var($dataArray['password'], FILTER_SANITIZE_STRING);
+
+        if (isset($dataArray['user_level']))
+        {
+            $this->$dataArray['user_level'] = filter_var($dataArray['user_level'], FILTER_SANITIZE_STRING);
+        }
+        
+
+        return $dataArray;
+    }
+
+     function load($user_id)
+    {
+
+        $isLoaded = false;
+        //load info from database
+        $stmt = $this->db->prepare("SELECT * FROM users WHERE user_id=?");
+        $stmt->execute(array($user_id));
+
+        if ($stmt->rowCount() == 1) {
+            $dataArray = $stmt->fetch(PDO::FETCH_ASSOC);
+            // var_dump($dataArray);
+            $this->set($dataArray);
+
+            $isLoaded = true;
+        }
+
+        // var_dump($stmt->rowCount());
+
+        return $isLoaded;
+    }
+
+    function save() 
+    {
+        $isSaved = false;
+        
+        // determine if insert or update based on user_id
+        // save data from userData property to database
+        if (empty($this->userData['user_id']))
+        {
+            $stmt = $this->db->prepare(
+                "INSERT INTO users 
+                    (userName, password, user_level) 
+                 VALUES (?, ?, ?)");
+               
+            $isSaved = $stmt->execute(array(
+                    $this->userData['userName'],
+                    $this->userData['password'],
+                    $this->userData['user_level']
+                )
+            );
+            
+            if ($isSaved) 
+            {
+                $this->userData['user_id'] = $this->db->lastInsertId();
+            }
+        } 
+        else 
+        {
+            $stmt = $this->db->prepare(
+                "UPDATE users SET 
+                    userName = ?,
+                    password = ?,
+                    user_level = ?
+                WHERE user_id = ?"
+            );
+                    
+            $isSaved = $stmt->execute(array(
+                    $this->userData['userName'],
+                    $this->userData['password'],
+                    $this->userData['user_level'],
+                    $this->userData['user_id']
+                )
+            );            
+        }
+                        
+        return $isSaved;
+    }
+
+     function validate()
+    {
+        $isValid = false;
+
+        // if an error, store to errors using column name as key
+
+        //validate the data elements and article data property
+        if (empty($this->userData['userName'])) {
+            $this->errors['userName'] = "Please enter a username";
+        }
+        if (empty($this->userData['password'])) {
+            $this->errors['password'] = "Please enter a password";
+        }
+        if (empty($this->userData['user_level'])) {
+            $this->errors['user_level'] = "Please enter a user level";
+        } 
+
+        if(empty($this->errors)){
+            $isValid = true;
+        }
+        return $isValid;
+    }
+
+     function getList($sortColumn = null, $sortDirection = null, $filterColumn = null, $filterText = null)
+    {
+        $articleList = array();
+        $sql = "SELECT * FROM Users ";
+
+        if(!is_null($filterColumn) && !is_null($filterText))
+        {
+            $sql .= "WHERE " . $filterColumn . " LIKE ?";
+        }
+
+        if(!is_null($sortColumn))
+        {
+            $sql .= "ORDER BY " . $sortColumn;
+            if(!is_null($sortDirection))
+            {
+                $sql .= " " . $sortDirection;
+            }
+        }
+        
+        $stmt = $this->db->prepare($sql);
+
+        if ($stmt) {
+            $stmt->execute(array('%' . $filterText . '%'));
+            
+            $articleList = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        return $articleList;
+    }
+
+    function checkLogin ($userName, $password) 
+    {
+        $user_id = "";
+        $stmt = $this->db->prepare(
+            "SELECT user_id FROM users
+            WHERE userName = ? && password = ?");
+
+            $stmt->execute(array($userName, $password));
+            $checkUserID = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($checkUserID == false) 
+            {
+                $this->errors['password'] = "password doesn't match username";
+            }
+
+            else 
+            {
+                $checkUserID = $checkUserID['user_id'];
+            }
+
+            return $checkUserID;
+    }
+}
