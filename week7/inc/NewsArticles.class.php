0092285eb68930e97edeecde0ee0577c6e85e121
webdevAndres <macias.andres02@gmail.com>
2018-04-09 08:15:02 -0500

    modified to include base class

--- week7/inc/NewsArticles.class.php
+++ week7/inc/NewsArticles.class.php
@@ -1,6 +1,156 @@
 <?php
+require_once('Base.class.php');
 
-class NewsArticles
+class NewsArticles extends Base
+{
+
+     function __construct()
+    {
+        // still run parent functionality
+       parent::__construct();
+
+       $this->tableName = "newsarticles";
+       $this->keyField = "articleID";
+       $this->columnNames = array( "articleTitle", "articleContent", "articleAuthor", "articleDate");
+    }
+
+
+     function sanitize($dataArray)
+    {
+        //sanitize the data based on rules
+         $dataArray['articleTitle'] = filter_var($dataArray['articleTitle'], FILTER_SANITIZE_STRING);
+        $dataArray['articleContent'] = filter_var($dataArray['articleContent'], FILTER_SANITIZE_STRING);
+        $dataArray['articleAuthor'] = filter_var($dataArray['articleAuthor'], FILTER_SANITIZE_STRING);
+        $dataArray['articleDate'] = filter_var($dataArray['articleDate'], FILTER_SANITIZE_STRING);
+
+        return $dataArray;
+    }
+
+
+     function validate()
+    {
+        $isValid = false;
+
+        // if an error, store to errors array using column name as key
+
+        //validate the data elements and article data property
+        if (empty($this->data['articleTitle'])) {
+            $this->errors['articleTitle'] = "Please enter a title";
+        }
+        if (empty($this->data['articleContent'])) {
+            $this->errors['articleContent'] = "Please enter Content";
+        }
+        if (empty($this->data['articleAuthor'])) {
+            $this->errors['articleAuthor'] = "Please enter an Author";
+        } 
+        
+        $year = substr($this->data['articleDate'], 0, 4);
+        $month = substr($this->data['articleDate'], 5,2);
+        $day = substr($this->data['articleDate'], 8,2);
+
+        if (empty($this->data['articleDate'])) {
+            $this->errors['articleDate'] = "Please enter a Date";
+        }
+        elseif (!checkdate($month,$day,$year)) {
+            $this->errors['articleDate'] = "Enter a valid date (yyyy-mm-dd)";
+            
+        }
+
+        if(empty($this->errors)){
+            $isValid = true;
+        }
+        return $isValid;
+    }
+
+    function export($xportFile) 
+    {
+        $success = true;
+
+        $fileHandle = fopen($exportFile, "w");
+
+        if($fileHandle)
+        {
+            $articleList = $this->getList();
+
+            $headerRow = false;
+            foreach ($articleList as $articleData)
+            {
+                if(!$headerRow)
+                {
+                    fputcsv($fileHandle, array_keys($articleData));
+                    $headerRow = true;
+                }
+
+                fputcsv($fileHandle, $articleData);
+               
+            }
+            fclose($fileHandle);
+
+        }
+        else
+        {
+            $success = false;
+        }
+        return $success;
+    }
+
+    function import($importFile)
+    {
+        $success = true;
+        
+        $fileHandle = fopen($importFile, "r");
+
+        if($fileHandle)
+        {
+            $headerColumns = array();
+
+            while (!feof($fileHandle))
+            {
+                $articleData = fgetscv($fileHandle);
+                if(empty($headerColumns))
+                {
+                    $headerColumns = $articleData;
+                }
+                else
+                {
+                    if(is_array($articleData))
+                    {
+                        $articleData = array_combine($headerColumns, $articlceData);
+
+                        $this->set($articleData);
+                        $this->save();
+                    }
+                   
+                }
+            }
+
+            fclose($fileHandle);
+
+        }
+        else
+        {
+            $success = false;
+        }
+        return $success;
+    }
+
+    function saveImage($imageFileData)
+    {
+        move_uploaded_file 
+        (
+            $imageFileData['tmp_name'], dirname(__FILE__) . "../public_html/images/article_" . $this->data[$this->keyField] . ".jpg"
+        );
+    }
+}
+
+
+
+
+
+
+
+
+class NewsArticles2
 {
     // using var is the same as public
     public $articleData = array();
@@ -165,3 +315,4 @@ class NewsArticles
         return $articleList;
     }
 }
+?>
