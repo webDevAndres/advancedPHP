3607e5de0012ed5ee3064f930962bc7a47049376
webdevAndres <macias.andres02@gmail.com>
2018-04-09 14:21:51 -0500

    added image to user view

--- week8/tpl/user-view.tpl.php
+++ week8/tpl/user-view.tpl.php
@@ -12,14 +12,23 @@
             height: 100vh;
             text-align: center;
         }
+        img {
+            width: 10%;
+            height: auto;
+            padding: 5px;
+            background-color: white;
+            border: 2px solid black;
+        }
     </style>
 
 </head>
 <body>
     <p><a href="user-edit.php?user_id=<?php echo $userDataArray['user_id'];?>">edit user</a></p>
     <p><a href="user-list.php">user list</a></p>
+        ProfileImage: <br>
+        <?php echo '<img src="../public_html/images/profile_'.$userDataArray['user_id'].'.jpg" alt="profile Image">'; ?>
+<br>
         Username: <?php echo (isset($userDataArray['userName']) ? $userDataArray['userName'] : ''); ?><br>
         user_level <?php echo (isset($userDataArray['user_level']) ? $userDataArray['user_level'] : ''); ?><br>
-        
     </body>
 </html>
