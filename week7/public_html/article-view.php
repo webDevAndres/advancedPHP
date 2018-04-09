3a737737adb93e6313e000479dd6862c21c00170
webdevAndres <macias.andres02@gmail.com>
2018-04-09 08:17:24 -0500

    modified article view

--- week7/public_html/article-view.php
+++ week7/public_html/article-view.php
@@ -10,7 +10,7 @@ $articleDataArray = array();
 if (isset($_REQUEST['articleID']) && $_REQUEST['articleID'] > 0) 
 {
     $newsArticle->load($_REQUEST['articleID']);
-    $articleDataArray = $newsArticle->articleData;
+    $articleDataArray = $newsArticle->data;
 }
 require_once('../tpl/article-view.tpl.php');
 ?>
